73af3719d0a8c09f44f98e9553639d94fdd87f5c
smartbitnl <smartbitnl@users.noreply.github.com>
2015-11-07 15:43:26 +0100

    Create fedora23.sh
    installing docker on fedora23, fails for second VM unless  docker.service adapted

---
+++ fedora23.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+export LANG=en_US # for dnf
+# Nov 7,'15 fails on fedora23: curl -sSL https://get.docker.com/ | sh
+cat >/etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/fedora/22
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+# optionally export DOCKER_OPTS="--bip=172.18.0.0/16"
+dnf install -yq python2 python2-dnf libselinux-python unzip nano docker-engine
+# nano /usr/lib/systemd/system/docker.service & modify ExecStart=/usr/bin/docker daemon -H fd:// --bip=172.18.0.0/16 
+systemctl start docker # will take some time & will fail on secodn machine unless --bip :-/
+systemctl enable docker #boot at system time
+docker version
+# docker network inspect bridge
+# cat /usr/lib/systemd/system/docker.service
