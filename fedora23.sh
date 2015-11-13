f40cf8d6f1a49f87611411e8d3d46679a042ab80
smartbitnl <smartbitnl@users.noreply.github.com>
2015-11-13 16:22:34 +0100

    Update fedora23.sh

--- fedora23.sh
+++ fedora23.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 export LANG=en_US # for dnf
+echo 'export $LANG=en_US' >> /etc/profile.d/custom.sh
 # Nov 7,'15 fails on fedora23: curl -sSL https://get.docker.com/ | sh
 # see http://docs.docker.com/machine/install-machine/
 cat >/etc/yum.repos.d/docker.repo <<-EOF
@@ -13,19 +14,19 @@ EOF
 dnf install -yq python2 python2-dnf libselinux-python unzip nano docker-engine wget bridge-utils
 systemctl start docker  # will take some time & might fail if VBox guest additions are not installed: "vagrant plugin install vagrant-vbguest"
 systemctl enable docker # boot at system time
-docker pull calico/node:v0.10.0
-docker pull calico/node-libnetwork:v0.5.2
-docker pull busybox
-docker pull mysql
-docker pull centos
+docker pull calico/node:v0.10.0 > /dev/null
+docker pull calico/node-libnetwork:v0.5.2 > /dev/null
+docker pull busybox > /dev/null
+docker pull mysql > /dev/null
+docker pull centos > /dev/null
 curl -L --silent https://github.com/projectcalico/calico-docker/releases/download/v0.10.0/calicoctl -o /usr/bin/calicoctl # latest build @ http://www.projectcalico.org/latest/calicoctl
 chmod +x /usr/bin/calicoctl # see http://www.projectcalico.org/docker-libnetwork-is-almost-here-and-calico-is-ready/
 # see http://www.projectcalico.org/docker-libnetwork-is-almost-here-and-calico-is-ready/ ; latest etcd @ https://github.com/coreos/etcd/releases/
-curl -L --silent https://github.com/coreos/etcd/releases/download/v2.2.1/etcd-v2.2.1-linux-amd64.tar.gz -o etcd-v2.2.1-linux-amd64.tar.gz
-tar xzf etcd-v2.2.1-linux-amd64.tar.gz
-cd etcd-v2.2.1-linux-amd64
+curl -L --silent https://github.com/coreos/etcd/releases/download/v2.2.1/etcd-v2.2.1-linux-amd64.tar.gz -o etcd-linux-amd64.tar.gz
+tar xzf etcd-linux-amd64.tar.gz --exclude Documentation --exclude '*.md' -C /usr/bin --strip-components=1
 # this needs some work
-./etcd --advertise-client-urls http://10.0.2.15:4001 --listen-client-urls http://0.0.0.0:4001
+# ./etcd --advertise-client-urls http://10.0.2.15:4001 --listen-client-urls http://0.0.0.0:4001
 docker version
+etcd --version
 # docker network inspect bridge
 # cat /usr/lib/systemd/system/docker.service
